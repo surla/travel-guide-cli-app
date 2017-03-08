49dd13503c00879b73cd14ce87804b929898724f
Joseph Surla <josephsurla@gmail.com>
2017-03-08 09:39:48 -0800

    #doc to obtain city details and complete city methods with details from travel guide

--- lib/city.rb
+++ lib/city.rb
@@ -4,8 +4,9 @@ class City
                   :best_time_to_visit,
                   :transportation,
                   :weather,
-                  :know_before_visting,
+                  :know_before_visiting,
                   :language,
+                  :electric,
                   :currency
 
   @@all = []
@@ -16,8 +17,41 @@ class City
     @@all << self
   end
 
+
   def self.all
     @@all
+  end
+
+  def doc
+    @doc = Nokogiri::HTML(open("#{url}"))
+  end
+
+  def best_time_to_visit
+    doc.css("p.article-tips__content").first.text
+  end
+
+  def transportation
+    doc.css("p.article-tips__content")[1].text
+  end
+
+  def weather
+    doc.css("p.article-tips__content")[2].text
+  end
+
+  def know_before_visiting
+    doc.css("p.article-tips__content")[3].text.strip
+  end
+
+  def language
+    doc.css("p.article-tips__content")[4].text
+  end
+
+  def electric
+    doc.css("p.article-tips__content")[5].text
+  end
+
+  def currency
+    doc.css("p.article-tips__content")[6].text
   end
 
 
