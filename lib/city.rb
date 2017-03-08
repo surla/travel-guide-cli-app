20b8bbf7153a85c59e940cbb464134f0d22151e7
Joseph Surla <josephsurla@gmail.com>
2017-03-08 09:42:35 -0800

    Refactor city object to include private methods

--- lib/city.rb
+++ lib/city.rb
@@ -22,37 +22,46 @@ class City
     @@all
   end
 
-  def doc
-    @doc = Nokogiri::HTML(open("#{url}"))
-  end
+
 
   def best_time_to_visit
-    doc.css("p.article-tips__content").first.text
+    details.first.text
   end
 
   def transportation
-    doc.css("p.article-tips__content")[1].text
+    details[1].text
   end
 
   def weather
-    doc.css("p.article-tips__content")[2].text
+    details[2].text
   end
 
   def know_before_visiting
-    doc.css("p.article-tips__content")[3].text.strip
+    details[3].text.strip
   end
 
   def language
-    doc.css("p.article-tips__content")[4].text
+    details[4].text
   end
 
   def electric
-    doc.css("p.article-tips__content")[5].text
+    details[5].text
   end
 
   def currency
-    doc.css("p.article-tips__content")[6].text
+    details[6].text
   end
+
+  private
+    def doc
+      @doc = Nokogiri::HTML(open("#{url}"))
+    end
+
+    def details
+      doc.css("p.article-tips__content")
+    end
+
+
 
 
 
