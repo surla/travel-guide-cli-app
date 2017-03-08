6a831699e16dda512da73765481e2d426412d43c
Joseph Surla <josephsurla@gmail.com>
2017-03-08 09:22:39 -0800

    Create scraper object and include #scrape_cities to obtain names of cities

---
+++ lib/scraper.rb
@@ -0,0 +1,16 @@
+require 'open-uri'
+
+class Scraper
+  def doc
+    @doc = Nokogiri::HTML(open("http://www.travelandleisure.com/travel-guide"))
+  end
+
+  def scrape_cities
+    doc.css("div.grid__item div.grid__item__details").each do |city|
+      name = city.css(".grid__item__title").text
+      url = "http://www.travelandleisure.com/local-experts/#{name}"
+      City.new(name, url)
+    end
+  end
+
+end
